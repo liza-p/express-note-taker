877c03620e8bd65ffc6167efbe9e20fa14bf5d2f
liza p <liyourlp@gmail.com>
2020-07-17 16:33:56 -0700

    add post rout

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var fs = require('fs');
+var path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 var app = express();
 var PORT = 3000;
@@ -7,36 +9,49 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-function handleRequest(fileName, res){ 
-    fs.readFile(__dirname + "/public/" + fileName, function(err, data) {
-        if (err) throw err;
-        
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-}
 
-app.get("/notes", function(req, res) {
-    handleRequest("notes.html",res)
-});
-app.get("/api/notes", function(req, res) {
-    fs.readFile(__dirname + "/db/db.json", function(err,data) {
-        if (err) throw err;
-        
-        res.json(JSON.parse(data));
-      });
-});
-    
-app.get("*", function(req, res) {
-    handleRequest("index.html",res)
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    console.log(JSON.parse(data));
+    res.json(JSON.parse(data));
+  });
+});
 
+app.post("/api/notes", function (req, res) {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  // Grab it from db.json and parse it into an array
+  const notes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json", function (err, data) {
+    return data;
+  }));
 
+  console.log(notes);
 
+  // Push your new note (aka req.body) into that parsed array
+  notes.push(newNote);
+  res.json(notes);
 
-app.listen(PORT, function() {
+  // Then rewrite to db.json and overwrite whatever is in there
+  fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
+    if (err) throw err;
+    console.log("Done!")
+  });
+
+});
+app.delete()
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
